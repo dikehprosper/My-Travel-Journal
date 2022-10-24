b0f2c14af297defac34cb1b51d84de6ff5430275
dikehprosper <103826657+dikehprosper@users.noreply.github.com>
2022-10-24 12:15:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Hero from './components/Hero';
+import Herodata from './components/Herodata';
+import Nav from './components/Nav';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const AllHeroData = Herodata.map( item => {
+    return (
+    <Hero 
+    key={item.id}
+    {...item}
+    />)
+  })
+    
+return (
+  <div className="main_body">
+    <Nav />
+    {AllHeroData}
+  </div>
+)
 
+}
 export default App;
---
+++ src/components/Herodata.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id : 1,
+        img: "/images/bananasland.jpg",
+        state : "LAGOS",
+        googleMap : "https://www.google.com/search?q=google+map+location+of+lagos+island&oq=google+map+location+of+lagos+island&aqs=chrome..69i57j69i64.11662j0j7&sourceid=chrome&ie=UTF-8",
+        location : "BANANA ISLAND",
+        date : "12 Jan 2018 - 13 Jan 2019",
+        description : "BANANA ISLAND is a developed part of lagos state,inhabited majorly by rich and wealthy people. It is really expensive to stay in this region as houses are as high as $2M to purchase.",
+    },
+    {
+        id : 2,
+        img: "/images/owerricapital.jpeg",
+        state : "IMO",
+        googleMap : "https://www.google.com/search?q=google+map+location+of+owerri&sxsrf=ALiCzsa2cigR_COkLdkJdOc7FmsMazWnXw%3A1666543184711&ei=UG5VY63xKo__7_UPwOqzwAQ&ved=0ahUKEwit2ZuU5fb6AhWP_7sIHUD1DEgQ4dUDCA4&uact=5&oq=google+map+location+of+owerri&gs_lcp=Cgdnd3Mtd2l6EAxKBAhBGABKBAhGGABQAFgAYABoAHAAeACAAQCIAQCSAQCYAQA&sclient=gws-wiz",
+        location : "OWERRI",
+        date : "20 Dec 2019 - 13 Jan 2020",
+        description : "Owerri is the capital of Imo state,inhabited majorly by wealthy and average people. It is populated with lots of hotels.",
+    },
+    {
+        id : 3,
+        img: "/images/Port-Harcourt-City.jpg",
+        state : "RIVERS",
+        googleMap : "https://wwhttps://www.google.com/search?q=google+map+location+of+portharcourt&sxsrf=ALiCzsbLaYfdg8Es5B5ouJmk6niMgKo9Ig%3A1666543724356&ei=bHBVY4eyFaqU9u8P7rmagA0&oq=google+map+location+of+portharcou&gs_lcp=Cgdnd3Mtd2l6EAEYADIFCCEQoAEyBQghEKABOgoIABBHENYEELADOgYIABAWEB46BAgjECc6CAgAEIAEEMsBOggIIRAWEB4QHUoECEEYAEoECEYYAFCNEFjSO2CHT2gCcAF4AIAB6QKIAYcakgEFMi05LjOYAQCgAQHIAQjAAQE&sclient=gws-wizw.google.com/search?q=google+map+location+of+lagos+island&oq=google+map+location+of+lagos+island&aqs=chrome..69i57j69i64.11662j0j7&sourceid=chrome&ie=UTF-8",
+        location : "PORT-HARCOURT",
+        date : "13 Jan 2020 - 13 Oct 2022",
+        description : "PORT-HARCOURT is a developed part of river state. The roads are well-built and lots of bridges at every major junction.",
+    },
+  
+]
